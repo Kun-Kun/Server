a1315d8bc703f4da074b510692ec7e7585ddac06
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-03-08 23:04:59 +0200

    Test changed, clear meaningless code in handlers,tests,and routers

--- softgroup/services/common/common-impl/src/test/java/com/softgroup/common/impl/router/CommonTypeRouterUnitTest.java
+++ softgroup/services/common/common-impl/src/test/java/com/softgroup/common/impl/router/CommonTypeRouterUnitTest.java
@@ -4,21 +4,19 @@ package com.softgroup.common.impl.router;
  * Created by user on 03.03.2017.
  */
 
-import com.softgroup.authorization.api.message.RegisterRequest;
-import com.softgroup.authorization.impl.router.AuthorizationRouter;
 import com.softgroup.common.protocol.ActionHeader;
 import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.RequestData;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.router.api.CommandTypeRouter;
 
-import com.softgroup.profile.api.message.GetContactProfilesRequest;
 
-import com.softgroup.profile.impl.router.ProfileRouter;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
@@ -27,6 +25,7 @@ import java.util.HashMap;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
@@ -45,34 +44,34 @@ public class CommonTypeRouterUnitTest {
     private ArrayList<CommandTypeRouter> handlers;
     @Spy
     private HashMap<String,CommandTypeRouter> handlersMap;
-    @Spy
-    private AuthorizationRouter authorizationRouter;
-    @Spy
-    private ProfileRouter profileRouter;
+    @Mock
+    private CommandTypeRouter authorizationRouter;
+    @Mock
+    private CommandTypeRouter profileRouter;
 
     @Test
     public void isRouterExist(){
         assertThat(router, CoreMatchers.notNullValue());
     }
 
-    private Request<RegisterRequest> authorizationRequestREST;
-    private Request<GetContactProfilesRequest> profileRequestREST;
+    private Request<RequestData> authorizationRequestREST;
+    private Request<RequestData> profileRequestREST;
 
     @Before
     public void createRequestResponse(){
-        authorizationRequestREST = new Request<RegisterRequest>();
+        authorizationRequestREST = new Request<RequestData>();
         ActionHeader header = new ActionHeader();
         header.setType("authorization");
-        RegisterRequest registerEntry = new RegisterRequest();
         authorizationRequestREST.setHeader(header);
-        authorizationRequestREST.setData(registerEntry);
 
-        profileRequestREST = new Request<GetContactProfilesRequest>();
+        when(authorizationRouter.getName()).thenReturn("authorization");
+
+        profileRequestREST = new Request<RequestData>();
         ActionHeader header1 = new ActionHeader();
         header1.setType("profile");
-        GetContactProfilesRequest getContactProfilesEntry = new GetContactProfilesRequest();
         profileRequestREST.setHeader(header1);
-        profileRequestREST.setData(getContactProfilesEntry);
+
+        when(profileRouter.getName()).thenReturn("profile");
 
         handlers.add(authorizationRouter);
         handlers.add(profileRouter);
@@ -86,16 +85,15 @@ public class CommonTypeRouterUnitTest {
     @Test
     public void traceRouteToAuth(){
         Response r = router.handle(authorizationRequestREST);
-
         verify(authorizationRouter).handle(authorizationRequestREST);
-        verify(authorizationRouter,never()).handle(profileRequestREST);
+        verify(profileRouter,never()).handle(authorizationRequestREST);
     }
 
     @Test
     public void traceRouteToProfiles(){
         Response r = router.handle(profileRequestREST);
         verify(profileRouter).handle(profileRequestREST);
-        verify(profileRouter,never()).handle(authorizationRequestREST);
+        verify(authorizationRouter,never()).handle(profileRequestREST);
     }
 
 
