2e839d821e50ac146e03c50ca7bfec1569a3c2fe
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-03-15 11:14:11 +0200

    AuthorizationAppCfg filename fixed, move to another package.

--- softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorization/impl/config/AuthorizationAppCfg.java
+++ softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorization/impl/config/AuthorizationAppCfg.java
@@ -1,4 +1,4 @@
-package com.softgroup.authorization.impl.router;
+package com.softgroup.authorization.impl.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan("com.softgroup.authorization")
-public class AuthorizationRouterAppCfg {
+public class AuthorizationAppCfg {
 
 }
