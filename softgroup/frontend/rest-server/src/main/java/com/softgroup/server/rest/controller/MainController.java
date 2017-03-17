9138306c54f44f6b0a8f1a58ebea8409b49cf556
maxim <maximmazurenko@rambler.ru>
2017-03-17 13:34:55 +0200

    Removed Import Annotation

--- softgroup/frontend/rest-server/src/main/java/com/softgroup/server/rest/controller/MainController.java
+++ softgroup/frontend/rest-server/src/main/java/com/softgroup/server/rest/controller/MainController.java
@@ -1,14 +1,11 @@
 package com.softgroup.server.rest.controller;
 
-import com.softgroup.common.datamapper.configuration.DataMapperAppCfg;
 import com.softgroup.common.protocol.Response;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/api")
-@Import(DataMapperAppCfg.class)
 public class MainController {
 
     @RequestMapping(value = "/private",method = RequestMethod.POST,consumes="application/json",produces = "application/json")
