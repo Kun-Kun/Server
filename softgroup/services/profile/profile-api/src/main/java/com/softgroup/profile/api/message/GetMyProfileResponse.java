403f836cc91eab91b4afcb472b48152090619929
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-03-26 23:58:56 +0300

    fix stored type in get my profile response

--- softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileResponse.java
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/GetMyProfileResponse.java
@@ -3,8 +3,6 @@ package com.softgroup.profile.api.message;
 import com.softgroup.common.protocol.ResponseData;
 import com.softgroup.common.protocol.DTO.DTOProfile;
 
-import java.util.List;
-
 /**
  * Created by user on 26.02.2017.
  */
@@ -12,13 +10,13 @@ public class GetMyProfileResponse implements ResponseData {
 
     private static final long serialVersionUID = 3465556264906202483L;
 
-    private List<DTOProfile> profile;
+    private DTOProfile profile;
 
-    public List<DTOProfile> getProfile() {
+    public DTOProfile getProfile() {
         return profile;
     }
 
-    public void setProfile(List<DTOProfile> profile) {
+    public void setProfile(DTOProfile profile) {
         this.profile = profile;
     }
 }
