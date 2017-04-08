86bc37da1a24801b95517f757f822a37a23a607a
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-04-08 23:05:45 +0300

    Changed serialization with using @JsonValue and JsonCreator

--- softgroup/services/messenger/messenger-api/src/main/java/com/softgroup/messenger/api/dto/enumeration/MessageType.java
+++ softgroup/services/messenger/messenger-api/src/main/java/com/softgroup/messenger/api/dto/enumeration/MessageType.java
@@ -1,18 +1,46 @@
 package com.softgroup.messenger.api.dto.enumeration;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by user on 26.03.2017.
  */
 public enum  MessageType {
-    @JsonProperty("0")
-    TEXT,
-    @JsonProperty("1")
-    IMAGE,
-    @JsonProperty("2")
-    LINK,
-    @JsonProperty("3")
-    SERVICE
+    TEXT(0),
+    IMAGE(1),
+    LINK(2),
+    SERVICE(3);
+
+    private Integer value;
+
+    private static final Map<Integer,MessageType> TYPE_MAP;
+
+    static {
+        Map<Integer,MessageType> map = new HashMap<>();
+        for (MessageType messageType : MessageType.values()) {
+            map.put(messageType.getValue(), messageType);
+        }
+        TYPE_MAP = Collections.unmodifiableMap(map);
+    }
+
+
+    @JsonValue
+    public Integer getValue() {
+        return value;
+    }
+
+    MessageType (Integer value){
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static MessageType fromValue(final Integer value) {
+        return TYPE_MAP.get(value);
+    }
 
 }
