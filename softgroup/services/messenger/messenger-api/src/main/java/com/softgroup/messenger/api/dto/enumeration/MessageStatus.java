43c816c4e56ecdd10f26a1fbe94111499ac38aad
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-04-08 23:05:45 +0300

    Changed serialization with using @JsonValue and JsonCreator

--- softgroup/services/messenger/messenger-api/src/main/java/com/softgroup/messenger/api/dto/enumeration/MessageStatus.java
+++ softgroup/services/messenger/messenger-api/src/main/java/com/softgroup/messenger/api/dto/enumeration/MessageStatus.java
@@ -1,19 +1,46 @@
 package com.softgroup.messenger.api.dto.enumeration;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by user on 26.03.2017.
  */
 public enum MessageStatus {
-    @JsonProperty("0")
-    GENERATED,
-    @JsonProperty("1")
-    SENT,
-    @JsonProperty("2")
-    DELIVERED,
-    @JsonProperty("3")
-    VIEWED,
-    @JsonProperty("4")
-    DELETED
+    GENERATED(0),
+    SENT(1),
+    DELIVERED(2),
+    VIEWED(3),
+    DELETED(4);
+
+    private Integer value;
+
+    private static final Map<Integer,MessageStatus> STATUS_MAP;
+
+    static {
+        Map<Integer,MessageStatus> map = new HashMap<>();
+        for (MessageStatus messageStatus : MessageStatus.values()) {
+            map.put(messageStatus.getValue(), messageStatus);
+        }
+        STATUS_MAP = Collections.unmodifiableMap(map);
+    }
+
+
+    @JsonValue
+    public Integer getValue() {
+        return value;
+    }
+
+    MessageStatus (Integer value){
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static MessageStatus fromValue(final Integer value) {
+        return STATUS_MAP.get(value);
+    }
 }
