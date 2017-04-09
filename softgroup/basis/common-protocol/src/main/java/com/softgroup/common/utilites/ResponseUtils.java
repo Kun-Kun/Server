c805b90166ab8dc9a08b857ec84063fbea25a992
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-04-09 13:49:31 +0300

    Added ResponseBuilder utility.

---
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/utilites/ResponseUtils.java
@@ -0,0 +1,54 @@
+package com.softgroup.common.utilites;
+
+import com.softgroup.common.protocol.*;
+
+/**
+ * Created by user on 09.04.2017.
+ */
+public class ResponseUtils {
+
+    private ResponseUtils() {
+    }
+
+    public static <T extends ResponseData> Response<T> createResponseFromRequest(Request<?> request, ResponseStatus status, T data){
+         return new ResponseBuilder<T>()
+                 .setData(data)
+                 .setHeader(request.getHeader())
+                 .setStatus(status).build();
+    }
+
+    public static <T extends ResponseData> Response<T> createResponseFromRequest(Request<?> request, ResponseStatusCode status, T data){
+        ResponseStatus responseStatus = new ResponseStatus();
+        responseStatus.setMessage(status.getMessage());
+        responseStatus.setCode(status.getCode());
+
+        return createResponseFromRequest(request,responseStatus,data);
+    }
+
+    public static <T extends ResponseData> Response<T> createOKResponse(Request<?> request, T data){
+        return createResponseFromRequest(request, ResponseStatusCode.OK, data);
+    }
+
+    public static <T extends ResponseData> Response<T> createBadRequestResponse(Request<?> request){
+        return createResponseFromRequest(request, ResponseStatusCode.BAD_REQUEST, null);
+    }
+
+    public static <T extends ResponseData> Response<T> createErrorResponse(Request<?> request,ResponseStatusCode code){
+        if(code.equals(ResponseStatusCode.OK)){
+            throw new IllegalArgumentException("Error response can not have OK(200) code with empty data. You must use createOKResponse method.");
+        }
+        return createResponseFromRequest(request, code, null);
+    }
+
+    public static <T extends ResponseData> Response<T> createCustomResponse(Request<?> request,Integer code, String message){
+        ResponseStatus responseStatus = new ResponseStatus();
+        responseStatus.setMessage(message);
+        responseStatus.setCode(code);
+        return createResponseFromRequest(request,responseStatus,null);
+    }
+
+    public static <T extends ResponseData> Response<T> createCustomResponse(Request<?> request,ResponseStatusCode status, String additionMessage){
+        return createCustomResponse(request, status.getCode(), status.getMessage()+". "+ additionMessage);
+    }
+
+}
