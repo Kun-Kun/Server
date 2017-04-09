ec30a438cfdc8fe29d2e5597328bcf5240660f35
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-04-09 23:46:57 +0300

    Added bad request when factory return null.

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
@@ -3,6 +3,7 @@ package com.softgroup.common.router.api;
 import com.softgroup.common.factory.HandlerFactory;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.utilites.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractRouterHandler<T extends HandlerFactory> implements RouterHandler {
@@ -12,8 +13,11 @@ public abstract class AbstractRouterHandler<T extends HandlerFactory> implements
 
 	@Override
 	public Response<?> handle(Request<?> msg) {
-		return factory.getHandler(msg).handle(msg);
+		Handler handler = factory.getHandler(msg);
+		if (handler == null){
+			return ResponseUtils.createBadRequestResponse(msg);
+		}
+		return handler.handle(msg);
 	}
-
 
 }
