3f05463757eb142ffa3a9e8543d690e6c95bae7b
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-11-08 23:43:17 +0200

    Addedget my profile handler

--- softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetMyProfileRequestHandler.java
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetMyProfileRequestHandler.java
@@ -1,11 +1,17 @@
 package com.softgroup.profile.impl.handler;
 
+import com.softgroup.common.dao.api.entities.ProfileEntity;
+import com.softgroup.common.dao.impl.repositories.ProfileRepository;
+import com.softgroup.common.dto.DTOProfile;
+import com.softgroup.common.mapper.ProfileMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.common.utilites.ResponseUtils;
 import com.softgroup.profile.api.message.GetMyProfileRequest;
 import com.softgroup.profile.api.message.GetMyProfileResponse;
 import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +19,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class GetMyProfileRequestHandler extends AbstractRequestHandler<GetMyProfileRequest,GetMyProfileResponse>  implements ProfileRequestHandler {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileMapper profileMapper;
+
+    @Override
     public String getName(){
         return "get_my_profile";
     }
@@ -23,6 +37,11 @@ public class GetMyProfileRequestHandler extends AbstractRequestHandler<GetMyProf
     }
 
     public Response<GetMyProfileResponse> processRequest(Request<GetMyProfileRequest> msg){
-        return null;
+        String userId = msg.getRoutingData().getUserId();
+        ProfileEntity profile = profileRepository.findOne(userId);
+        DTOProfile dtoProfile = profileMapper.mapProfileDtoFromEntity(profile);
+        GetMyProfileResponse response = new GetMyProfileResponse();
+        response.setProfile(dtoProfile);
+        return ResponseUtils.createOKResponse(msg,response);
     }
 }
