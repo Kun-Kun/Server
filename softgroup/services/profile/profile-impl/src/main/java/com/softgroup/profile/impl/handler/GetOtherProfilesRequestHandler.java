23303482e0c52187de5fe1b596fe721f5f439d37
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-11-11 00:33:24 +0200

    Added get other profiles handler

--- softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetOtherProfilesRequestHandler.java
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetOtherProfilesRequestHandler.java
@@ -1,18 +1,35 @@
 package com.softgroup.profile.impl.handler;
 
+import com.softgroup.common.dao.api.entities.ProfileEntity;
+import com.softgroup.common.dto.DTOProfile;
+import com.softgroup.common.mapper.ProfileMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.common.utilites.ResponseUtils;
 import com.softgroup.profile.api.message.GetOtherProfilesRequest;
 import com.softgroup.profile.api.message.GetOtherProfilesResponse;
 import com.softgroup.profile.api.router.ProfileRequestHandler;
+import com.softgroup.profile.impl.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by user on 26.02.2017.
  */
 @Component
 public class GetOtherProfilesRequestHandler extends AbstractRequestHandler<GetOtherProfilesRequest,GetOtherProfilesResponse>  implements ProfileRequestHandler {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @Autowired
+    private ProfileMapper profileMapper;
+
+    @Override
     public String getName(){
         return "get_other_profiles";
     }
@@ -23,6 +40,13 @@ public class GetOtherProfilesRequestHandler extends AbstractRequestHandler<GetOt
     }
 
     public Response<GetOtherProfilesResponse> processRequest(Request<GetOtherProfilesRequest> msg){
-        return null;
+        List<String> profileIds = msg.getData().getUserId();
+        List<ProfileEntity> profileEntities = profileService.getProfiles(profileIds);
+        List<DTOProfile> dtoProfiles = profileEntities.stream().map(entity ->
+            profileMapper.mapProfileDtoFromEntity(entity)
+        ).collect(Collectors.toList());
+        GetOtherProfilesResponse response = new GetOtherProfilesResponse();
+        response.setProfiles(dtoProfiles);
+        return ResponseUtils.createOKResponse(msg,response);
     }
 }
